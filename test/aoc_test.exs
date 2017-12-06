4c4850b5088b91594526c4ead0f45b4448d1d3d4
Eduardo Moraga <o0eversor0o@gmail.com>
2017-12-05 22:00:42 -0300

    part 2 tests

--- test/aoc_test.exs
+++ test/aoc_test.exs
@@ -1,6 +1,5 @@
 defmodule AoCTest do
   use ExUnit.Case
-  doctest AoC
 
   test "greets the world" do
     assert AoC.hello() == :world
@@ -16,5 +15,25 @@ defmodule AoCTest do
 
   test "inverse captcha of 1111 is 4" do
     assert AoC.Day1.reverse_captcha("1111") == 4
+  end
+
+  test "inverse captcha of 1212 is 6 using halfway around pairing" do
+    assert AoC.Day1.reverse_captcha("1212", :halfway_around) == 6
+  end
+
+  test "inverse captcha of 1221 is 0 using halfway around pairing" do
+    assert AoC.Day1.reverse_captcha("1221", :halfway_around) == 0
+  end
+
+  test "inverse captcha of 123425 is 4 using halfway around pairing" do
+    assert AoC.Day1.reverse_captcha("123425", :halfway_around) == 4
+  end
+
+  test "inverse captcha of 123123 is 12 using halfway around pairing" do
+    assert AoC.Day1.reverse_captcha("123123", :halfway_around) == 12
+  end
+
+  test "inverse captcha of 12131415 is 4 using halfway around pairing" do
+    assert AoC.Day1.reverse_captcha("12131415", :halfway_around) == 4
   end
 end
